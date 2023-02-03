25a4a7da983d1e0b2b214d97e3183f717f99a650
hene <henechen@gmail.com>
2023-02-03 18:26:46 -0300

    Criado o evento que detecta quando a pessoa sai do voice channel

--- src/main/java/br/com/mundi/events/LeaveVoiceChannel.java
+++ src/main/java/br/com/mundi/events/LeaveVoiceChannel.java
@@ -1,2 +1,33 @@
-package br.com.mundi.events;public class LeaveVoiceChannel {
+package br.com.mundi.events;
+
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class LeaveVoiceChannel extends ListenerAdapter {
+
+    /// <Summary>
+    /// Evento que verifica quando uma pessoa entrou em um voice channel e informa qual voice channel ela entrou
+    /// </Summary>
+    @Override
+    public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
+
+        VoiceChannel voiceChannel = (VoiceChannel) event.getChannelLeft();
+
+        /// <Summary>
+        /// Seta o canal de logs do Discord.
+        /// Passado manualmente
+        /// </Summary>
+        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1067806975401918475L);
+
+        /// <Summary>
+        /// Verifica tanto se o canal de logs quanto o canal de voz existem
+        /// E caso seja true, retorna o log.
+        /// </Summary>
+        if ( canalDeLogs != null && voiceChannel != null) {
+
+            canalDeLogs.sendMessage(event.getMember().getAsMention() + " saiu do canal de voz " + voiceChannel.getAsMention()).queue();
+        }
+    }
 }
