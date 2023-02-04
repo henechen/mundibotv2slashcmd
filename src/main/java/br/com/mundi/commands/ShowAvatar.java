706fb5673d19e7a14f1aa62d21d62bc199d2cf4f
hene <henechen@gmail.com>
2023-02-04 16:42:43 -0300

    Adicionado comando para mostrar avatar próprio ou de outro usuário + correção de ortografia.

--- src/main/java/br/com/mundi/commands/ShowAvatar.java
+++ src/main/java/br/com/mundi/commands/ShowAvatar.java
@@ -12,28 +12,28 @@ public class ShowAvatar extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
 
-        /// <Summary>
+        /// <summary>
         /// O comando verifica se a palavra depois do / é "avatar", e caso seja segue com o comando
-        /// </Summary>
+        /// </summary>
         if ( event.getName().equals("avatar") ) {
 
-            /// <Summary>
+            /// <summary>
             /// Adicionado a opção para passar um "user".
-            /// </Summary>
+            /// </summary>
             OptionMapping user = event.getOption("user");
 
-            /// <Summary>
+            /// <summary>
             /// Como o comando nao é instântaneo, deferReply() foi adicionado para o bot conseguir lidar com o comando.
-            /// </Summary>
+            /// </summary>
             event.deferReply().queue();
 
-            /// <Summary>
+            /// <summary>
             /// O comando verifica se o usuário passado é nulo, e caso seja retorna o avatar de quem chamou o comando.
             /// "?size=4096" é adicionado junto método de pegar o avatar porque eu fui incapaz
             /// De achar um método que retornasse o avatar com seu tamanho maior
             /// PS: era pra ser o "getEffectiveAvatarUrl()" mas não funcionou não sei porque
             /// Então vai ficar assim mesmo.
-            /// </Summary>
+            /// </summary>
             if ( user == null ) {
 
                 String authorAvatar = event.getUser().getEffectiveAvatarUrl();
@@ -42,13 +42,13 @@ public class ShowAvatar extends ListenerAdapter {
             }
 
 
-            /// <Summary>
+            /// <summary>
             /// O comando verifica se o usuário passado não é nulo, e caso não seja retorna o avatar do usuário mencionado.
             /// "?size=4096" é adicionado junto método de pegar o avatar porque eu fui incapaz
             /// De achar um método que retornasse o avatar com seu tamanho maior
             /// PS: era pra ser o "getEffectiveAvatarUrl()" mas não funcionou não sei porque
             /// Então vai ficar assim mesmo.
-            /// </Summary>
+            /// </summary>
             if ( user != null ) {
 
                 String mentionedUser = user.getAsUser().getEffectiveAvatarUrl();
