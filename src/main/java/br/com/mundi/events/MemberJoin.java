6e46b4e424b7f5176ec1a905e045a999403db4b5
hene <henechen@gmail.com>
2023-02-08 04:07:30 -0300

    Mesma coisa do MemberLeave :)

--- src/main/java/br/com/mundi/events/MemberJoin.java
+++ src/main/java/br/com/mundi/events/MemberJoin.java
@@ -1,42 +1,40 @@
 package br.com.mundi.events;
 
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
-import java.util.Date;
 
 public class MemberJoin extends ListenerAdapter {
 
-    @Override
-    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+    /// <summary>
+    /// Comentei o código inteiro pois o mesmo foi implantando
+    /// Em forma de comando na pasta "commands/SetJoinChannel"
+    /// </summary>
 
-        /// <summary>
-        /// Identifica o nome do membro que entrou no canal.
-        /// </summary>
-        String joinedMember = event.getUser().getAsMention();
-
-        /// <summary>
-        /// ID do canal que mostrará a pessoa que entrou nos servidor.
-        /// </summary>
-        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1040589609836494859L);
-
-        /// <summary>
-        /// Verifica se o canal é nulo e caso não seja, retorna um embed com informações nenhum pouco importantes.
-        /// </summary>
-        if ( canalDeLogs != null ) {
-            EmbedBuilder embed = new EmbedBuilder()
-                    .setTitle("Bem-vindo")
-                    .setDescription("Que a sua estadia seja curta, " + joinedMember)
-                    .setColor(Color.BLACK)
-                    .setThumbnail("https://i.imgur.com/dgLBXe4.png")
-                    .setTimestamp(new Date().toInstant());
-
-            canalDeLogs.sendMessageEmbeds(embed.build()).queue();
-        }
-
-    }
+//    @Override
+//    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+//
+//        /// <summary>
+//        /// Identifica o nome do membro que entrou no canal.
+//        /// </summary>
+//        String joinedMember = event.getUser().getAsMention();
+//
+//        /// <summary>
+//        /// ID do canal que mostrará a pessoa que entrou nos servidor.
+//        /// </summary>
+//        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1040589609836494859L);
+//
+//        /// <summary>
+//        /// Verifica se o canal é nulo e caso não seja, retorna um embed com informações nenhum pouco importantes.
+//        /// </summary>
+//        if ( canalDeLogs != null ) {
+//            EmbedBuilder embed = new EmbedBuilder()
+//                    .setTitle("Bem-vindo")
+//                    .setDescription("Que a sua estadia seja curta, " + joinedMember)
+//                    .setColor(Color.BLACK)
+//                    .setThumbnail("https://i.imgur.com/dgLBXe4.png")
+//                    .setTimestamp(new Date().toInstant());
+//
+//            canalDeLogs.sendMessageEmbeds(embed.build()).queue();
+//        }
+//
+//    }
 }
