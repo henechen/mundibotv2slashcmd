46ae9c1d4ec77a932a3da4a27c4b2993ca3560fa
hene <henechen@gmail.com>
2023-02-08 04:07:52 -0300

    Criado comando que permite escolher qual canal você deseja adicionar o evento de log

---
+++ src/main/java/br/com/mundi/commands/SetLeaveChannel.java
@@ -0,0 +1,59 @@
+package br.com.mundi.commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.Date;
+
+public class SetLeaveChannel extends ListenerAdapter {
+
+    long currChannel;
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+
+        if (event.getName().equals("setleavechannel")) {
+
+            currChannel = event.getChannel().getIdLong();
+
+            event.reply("O canal " + event.getChannel().getAsMention() + " foi setado como canal de leave logs.").queue();
+        }
+    }
+
+    @Override
+    public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
+
+
+        /// <summary>
+        /// Identifica o nome do membro que saiu do servidor.
+        /// </summary>
+        String leftMember = event.getUser().getAsMention();
+
+        /// <summary>
+        /// ID do canal que mostrará a pessoa que saiu do servidor
+        /// O ID agora é captado com o slash command acima.
+        /// Valeu serpinha seu lindo <3
+        /// </summary>
+        TextChannel canalDeLogs = event.getGuild().getTextChannelById(currChannel);
+
+        /// <summary>
+        /// Verifica se o canal é nulo e caso não seja, retorna um embed com informações nenhum pouco importantes.
+        /// </summary>
+        if ( canalDeLogs != null ) {
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle("Bem-vindo")
+                    .setDescription("Que pena que você foi embora " + leftMember + " :)")
+                    .setColor(Color.BLACK)
+                    .setThumbnail("https://i.imgur.com/dgLBXe4.png")
+                    .setTimestamp(new Date().toInstant());
+
+            canalDeLogs.sendMessageEmbeds(embed.build()).queue();
+        }
+    }
+}
+
