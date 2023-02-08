7ec2173b2bbb0a1ff4ab43c5e3252f8f454bfa37
hene <henechen@gmail.com>
2023-02-08 04:07:18 -0300

    MemberLeave colocado como comentário para fins de anotação de criação. Adicionado comando específico para adicionar o evento em qualquer canal.

--- src/main/java/br/com/mundi/events/MemberLeave.java
+++ src/main/java/br/com/mundi/events/MemberLeave.java
@@ -1,41 +1,39 @@
 package br.com.mundi.events;
 
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
-import java.util.Date;
 
 public class MemberLeave extends ListenerAdapter {
 
-    @Override
-    public void onGuildMemberRemove(@NotNull GuildMemberRemoveEvent event) {
+    /// <summary>
+    /// Comentei o código inteiro pois o mesmo foi implantando
+    /// Em forma de comando na pasta "commands/SetJoinChannel"
+    /// </summary>
 
-        /// <summary>
-        /// Identifica o nome do membro que saiu do servidor.
-        /// </summary>
-        String removedMember = event.getUser().getAsMention();
-
-        /// <summary>
-        /// ID do canal que mostrará a pessoa que saiu do Discord.
-        /// </summary>
-        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1040589609836494859L);
-
-        /// <summary>
-        /// Verifica se o canal é nulo e caso não seja, retorna um embed com informações nenhum pouco importantes.
-        /// </summary>
-        if ( canalDeLogs != null ) {
-            EmbedBuilder embed = new EmbedBuilder()
-                    .setTitle("Adios, fdp")
-                    .setDescription("Foi de Vasco e foi cedo, " + removedMember)
-                    .setColor(Color.BLACK)
-                    .setThumbnail("https://i.imgur.com/dgLBXe4.png")
-                    .setTimestamp(new Date().toInstant());
-
-            canalDeLogs.sendMessageEmbeds(embed.build()).queue();
-        }
-    }
+//    @Override
+//    public void onGuildMemberRemove(@NotNull GuildMemberRemoveEvent event) {
+//
+//        /// <summary>
+//        /// Identifica o nome do membro que saiu do servidor.
+//        /// </summary>
+//        String removedMember = event.getUser().getAsMention();
+//
+//        /// <summary>
+//        /// ID do canal que mostrará a pessoa que saiu do Discord.
+//        /// </summary>
+//        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1040589609836494859L);
+//
+//        /// <summary>
+//        /// Verifica se o canal é nulo e caso não seja, retorna um embed com informações nenhum pouco importantes.
+//        /// </summary>
+//        if ( canalDeLogs != null ) {
+//            EmbedBuilder embed = new EmbedBuilder()
+//                    .setTitle("Adios, fdp")
+//                    .setDescription("Foi de Vasco e foi cedo, " + removedMember)
+//                    .setColor(Color.BLACK)
+//                    .setThumbnail("https://i.imgur.com/dgLBXe4.png")
+//                    .setTimestamp(new Date().toInstant());
+//
+//            canalDeLogs.sendMessageEmbeds(embed.build()).queue();
+//        }
+//    }
 }
