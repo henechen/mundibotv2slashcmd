54f6089e169de4a5c7bffc1e727869fd84e1ae0f
hene <henechen@gmail.com>
2023-02-13 19:03:46 -0300

    Comentado o script de evento por causa da criação de comando

--- src/main/java/br/com/mundi/events/LeaveVoiceChannel.java
+++ src/main/java/br/com/mundi/events/LeaveVoiceChannel.java
@@ -1,8 +1,5 @@
 package br.com.mundi.events;
 
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class LeaveVoiceChannel extends ListenerAdapter {
@@ -10,24 +7,24 @@ public class LeaveVoiceChannel extends ListenerAdapter {
     /// <summary>
     /// Evento que verifica quando uma pessoa entrou em um voice channel e informa qual voice channel ela entrou
     /// </summary>
-    @Override
-    public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
-
-        VoiceChannel voiceChannel = (VoiceChannel) event.getChannelLeft();
-
-        /// <summary>
-        /// Seta o canal de logs do Discord.
-        /// Passado manualmente
-        /// </summary>
-        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1067806975401918475L);
-
-        /// <summary>
-        /// Verifica tanto se o canal de logs quanto o canal de voz existem
-        /// E caso seja true, retorna o log.
-        /// </summary>
-        if ( canalDeLogs != null && voiceChannel != null) {
-
-            canalDeLogs.sendMessage(event.getMember().getAsMention() + " saiu do canal de voz " + voiceChannel.getAsMention()).queue();
-        }
-    }
+//    @Override
+//    public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
+//
+//        VoiceChannel voiceChannel = (VoiceChannel) event.getChannelLeft();
+//
+//        /// <summary>
+//        /// Seta o canal de logs do Discord.
+//        /// Passado manualmente
+//        /// </summary>
+//        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1067806975401918475L);
+//
+//        /// <summary>
+//        /// Verifica tanto se o canal de logs quanto o canal de voz existem
+//        /// E caso seja true, retorna o log.
+//        /// </summary>
+//        if ( canalDeLogs != null && voiceChannel != null) {
+//
+//            canalDeLogs.sendMessage(event.getMember().getAsMention() + " saiu do canal de voz " + voiceChannel.getAsMention()).queue();
+//        }
+//    }
 }
