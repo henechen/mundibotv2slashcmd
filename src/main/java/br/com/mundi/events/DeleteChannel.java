369f902152c6e0df7c19d23eb2a7d0c10e8ba46c
hene <henechen@gmail.com>
2023-02-13 19:04:32 -0300

    Comentando o script de evento por causa do comando criado

--- src/main/java/br/com/mundi/events/DeleteChannel.java
+++ src/main/java/br/com/mundi/events/DeleteChannel.java
@@ -1,31 +1,28 @@
 package br.com.mundi.events;
 
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.events.channel.ChannelDeleteEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
 
 public class DeleteChannel extends ListenerAdapter {
 
-    @Override
-    public void onChannelDelete(@NotNull ChannelDeleteEvent event) {
-
-        /// <summary>
-        /// Identifica o nome do canal deletado.
-        /// </summary>
-        String nomeDoCanal = event.getChannel().getAsMention();
-
-        /// <summary>
-        /// ID do canal de logs para qual será enviado a mensagem de canal deletado.
-        /// </summary>
-        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1067806975401918475L);
-
-        /// <summary>
-        /// Verificação para assegurar que o canal de logs não foi deletado.
-        /// Resposta do bot para caso o canal seja deletado.
-        /// </summary>
-        if (canalDeLogs != null) {
-            canalDeLogs.sendMessage("O canal: " + nomeDoCanal + " foi deletado.").queue();
-        }
-    }
+//    @Override
+//    public void onChannelDelete(@NotNull ChannelDeleteEvent event) {
+//
+//        /// <summary>
+//        /// Identifica o nome do canal deletado.
+//        /// </summary>
+//        String nomeDoCanal = event.getChannel().getAsMention();
+//
+//        /// <summary>
+//        /// ID do canal de logs para qual será enviado a mensagem de canal deletado.
+//        /// </summary>
+//        TextChannel canalDeLogs = event.getGuild().getTextChannelById(1067806975401918475L);
+//
+//        /// <summary>
+//        /// Verificação para assegurar que o canal de logs não foi deletado.
+//        /// Resposta do bot para caso o canal seja deletado.
+//        /// </summary>
+//        if (canalDeLogs != null) {
+//            canalDeLogs.sendMessage("O canal: " + nomeDoCanal + " foi deletado.").queue();
+//        }
+//    }
 }
